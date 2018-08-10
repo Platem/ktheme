29198514822e0ec2d2dafbe61319637bfeaf61f3
Kevin Romero Peces-Barba <kevinrpb@hotmail.com>
2018-08-10 15:11:12 +0200

    Refactor repo for easier debug...

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ -f dist/me.kevinrpb.ktheme.deb ]; then
+  rm dist/me.kevinrpb.ktheme.deb
+fi
+
+dpkg -b build/me.kevinrpb.ktheme
+mv build/me.kevinrpb.ktheme.deb dist/me.kevinrpb.ktheme.deb
