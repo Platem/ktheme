8126cf9dca0254f81d4a068592fe707c5e55fc38
Kevin Romero Peces-Barba <kevinrpb@hotmail.com>
2018-08-10 15:14:57 +0200

    fix building script

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -f dist/me.kevinrpb.ktheme.deb ]; then
-  rm dist/me.kevinrpb.ktheme.deb
-fi
-
-dpkg -b build/me.kevinrpb.ktheme
-mv build/me.kevinrpb.ktheme.deb dist/me.kevinrpb.ktheme.deb
